41e662b53e0781d2429cc8d6b99092096fb41963
张亚飞 <672233580@qq.com>
2021-01-26 21:11:01 +0800

    Initial Commit

---
+++ ShimmerAnimation/ShimmerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerAnimationApp.swift
+//  ShimmerAnimation
+//
+//  Created by 张亚飞 on 2021/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
